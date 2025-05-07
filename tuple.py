9d0e70134f99cbf78816b46a4b3c6c6fa9aeb36a
idk3345445 <adityavardhankaw@gmail.com>
2025-05-07 20:26:44 +0400

    Initial commit

---
+++ tuple.py
@@ -0,0 +1,11 @@
+# Store personal details in a tuple
+
+personal_details_tuple = ("Adityavardhan", "kaw", 14, 176.9, 114, "Mathematics")
+
+# Convert the tuple into a list
+
+personal_details_list = list(personal_details_tuple)
+
+# Print the resulting list (optional)
+
+print(personal_details_list)
